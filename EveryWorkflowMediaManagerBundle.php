df8c4e39c0f3554a3afb4611d0c2575c061b6632
Suson Waiba <susonwaiba@gmail.com>
2021-11-21 05:03:17 +0000

    Added ?ExtensionInterface

--- EveryWorkflowMediaManagerBundle.php
+++ EveryWorkflowMediaManagerBundle.php
@@ -9,11 +9,12 @@ declare(strict_types=1);
 namespace EveryWorkflow\MediaManagerBundle;
 
 use EveryWorkflow\MediaManagerBundle\DependencyInjection\MediaManagerExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EveryWorkflowMediaManagerBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new MediaManagerExtension();
     }
