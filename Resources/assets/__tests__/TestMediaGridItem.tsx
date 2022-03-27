f481f108293b694a7bee0a6ac35ea2c1d2640dcd
Suson Waiba <susonwaiba@gmail.com>
2022-03-27 19:03:29 +0000

    Fixed test

--- Resources/assets/__tests__/TestMediaGridItem.tsx
+++ Resources/assets/__tests__/TestMediaGridItem.tsx
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import {cleanup, fireEvent, render} from '@testing-library/react';
-import MediaGridItem from "@EveryWorkflow/MediaManagerBundle/Component/MediaGridItem";
+import MediaGridItem from '@EveryWorkflow/MediaManagerBundle/Component/MediaGridItem';
 
 afterEach(cleanup);
 
@@ -12,7 +12,6 @@ test('Media manager --> grid item must show base_name on hover', () => {
     const result = render(
         <MediaGridItem itemData={{
             file_name: 'file_name',
-            base_name: 'base_name',
             extension: 'tsx',
             size: 55,
             path: 'path',
@@ -27,9 +26,9 @@ test('Media manager --> grid item must show base_name on hover', () => {
         }} />
     );
 
-    expect(result.queryByLabelText(/base_name/i)).toBeNull();
+    expect(result.queryByLabelText(/file_name/i)).toBeNull();
 
-    fireEvent.mouseOver(result.getByAltText(/base_name/i));
+    fireEvent.mouseOver(result.getByAltText(/file_name/i));
 
-    expect(result.queryByLabelText(/base_name/i)).toBeDefined();
+    expect(result.queryByLabelText(/file_name/i)).toBeDefined();
 });
